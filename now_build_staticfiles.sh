37319e29a8abf2d25dc4883aff38105531d662d7
anounymous <mranonymous2439@gmail.com>
2020-04-18 09:17:03 +0800

    changes in build file

--- now_build_staticfiles.sh
+++ now_build_staticfiles.sh
@@ -1,15 +1,15 @@
 #!/bin/bash
 
 # Install Python 3.6 since it is missing in the Now build environment
-yum install -y https://centos6.iuscommunity.org/ius-release.rpm
-yum install -y python36u
+#yum install -y https://centos6.iuscommunity.org/ius-release.rpm
+#yum install -y python36u
 
 # Install pip
-curl https://bootstrap.pypa.io/get-pip.py -o get-pip.py
-python3.6 get-pip.py
+#curl https://bootstrap.pypa.io/get-pip.py -o get-pip.py
+python3 get-pip.py
 
 # Install project requirements
 pip install -r requirements.txt
 
 # Build staticfiles
-# python manage.py collectstatic
+python manage.py collectstatic
